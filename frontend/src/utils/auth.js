cb1ac69238f58473ccf246d930b92dd91332a641
Svetlana Kaverina <lana.kaverina@internet.ru>
2023-09-01 13:04:16 +0300

    Заменяет ссылку на бек

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3000"; //заменить на доменное имя бэка
+export const BASE_URL = "https://api.mestokaverina.nomoredomainsicu.ru"; //заменить на доменное имя бэка
 
 function handleError(response) {
   if (!response.ok) {
